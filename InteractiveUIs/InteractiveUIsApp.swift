403804dfeeaef3c49ce70f601a20f768d721eabe
Emma Finkel <emma.finkel@gmail.com>
2023-04-05 10:27:31 -0400

    Initial Commit

---
+++ InteractiveUIs/InteractiveUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIsApp.swift
+//  InteractiveUIs
+//
+//  Created by scholar on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
